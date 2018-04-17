d56e560d575ba6f3649e0d1564eef0c53bb3fb46
Francesco Stefani <francesco.stefani@outlook.com>
2018-04-17 16:49:23 +0200

    Index done for Giacomo

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,7 +1,47 @@
 class BikesController < ApplicationController
 
+  before_action :set_bikes, only: [:show, :edit, :update, :destroy]
+
+
   def index
-    @bikes = Bike.all
+    @bikes = policy_scope(Bike)
+  end
+
+  def show
+  end
+
+  def new
+    @bike = current_user.bikes.new
+    authorize @bike
+  end
+
+  def create
+    @bike = current_user.bikes.new(bikes_params)
+    authorize @bike
+    if @bike.save
+      redirect_to bike_path(@bike)
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @bike.destroy
+    authorize @bike
+
+  end
+
+  def set_bikes
+    @bike = Bike.find(params[:id])
+    authorize @bike
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,80 @@
 #     Bike.new( make: Faker::Vehicle.manufacture, model:, description:, year: , category:, cc: round(rand(10)), consumption: rand(5), )
 #   end
 # end
+puts "resetting the seeds"
+User.destroy_all
+Bike.destroy_all
+Booking.destroy_all
+Profile.destroy_all
+
+# USERS
+user1 = User.new(email: "first@gmail.com", password: "1234567")
+user1.save
+user2 = User.new(email: "second@gmail.com", password: "1234567")
+user2.save
+user3 = User.new(email: "third@gmail.com", password: "1234567")
+user3.save
+user4 = User.new(email: "fourth@gmail.com", password: "1234567")
+user4.save
+
+# BIKES
+ducati = Bike.new(make: "Ducati", model: "Monster", description: "Good condition",
+        year: 2004, category: "Sport", cc: 250, rate: 50, user: user1 ,
+        photo: "Link to a photo", location: "Milan", consumption: 6.5)
+ducati.save
+yamaha = Bike.new(make: "Yamaha", model: "DoubleX", description: "Fast and steady",
+        year: 2015, category: "Sport", cc: 200, rate: 43, user: user1,
+        photo: "Link to a photo", location: "Brescia", consumption: 8)
+yamaha.save
+honda = Bike.new(make: "Honda", model: "Roadout", description: "For outdoor",
+        year: 1999, category: "Roadster", cc: 125, rate: 20, user: user2 ,
+        photo: "Link to a photo", location: "Bergamo", consumption: 7.6)
+honda.save
+harley = Bike.new(make: "Harley", model: "Luxe", description: "For long distance",
+        year: 2004, category: "Street", cc: 300, rate: 55, user: user3,
+        photo: "Link to a photo", location: "Milan", consumption: 8)
+harley.save
+kawasaki = Bike.new(make: "kawasaki", model: "OldStyle", description: "Old motorbike but good",
+        year:1980, category: "Sport", cc: 125, rate: 20, user: user4,
+        photo: "Link to a photo", location: "Crema", consumption: 10.7)
+kawasaki.save
+bmw = Bike.new(make: "BMW", model: "Roadout", description: "For forest and countryside",
+        year:2017, category: "outdoor", cc: 250, rate: 78, user: user4,
+        photo: "Link to a photo", location: "Crema", consumption: 7.3)
+bmw.save
+
+
+# BOOKINGS
+booking1 = Booking.new(start_date:"23-02-2018", end_date:"25-02-2018", status: 0,
+            user: user1 , bike: ducati)
+booking1.save
+booking2 = Booking.new(start_date:"30-11-2017", end_date:"12-12-2017", status: 2,
+            user: user1 , bike: yamaha)
+booking2.save
+booking3 = Booking.new(start_date:"03-09-2016", end_date:"17-09-2016", status: 1,
+            user: user2, bike: honda)
+booking3.save
+booking4 = Booking.new(start_date:"30-11-2017", end_date:"12-12-2017", status: 2,
+            user: user3, bike: harley)
+booking4.save
+booking5 = Booking.new(start_date:"26-01-2018", end_date:"28-02-2018", status: 0,
+            user: user4, bike: bmw)
+booking5.save
+
+
+# # PROFILES
+profile1 = Profile.new(name: "Paolo", photo: "Profile photo", bio: "Italian 32 years old",
+ experience: 12, user: user1)
+profile1.save
+profile2 = Profile.new(name: "Marco", photo: "Profile photo", bio: "German 28 years old",
+ experience: 10, user: user2)
+profile2.save
+profile3 = Profile.new(name: "Elena", photo: "Profile photo", bio: "Spanish 32 years old",
+ experience: 2, user: user3)
+profile3.save
+profile4 = Profile.new(name: "Paolo", photo: "Profile photo", bio: "Italian 45 years old",
+ experience: 12, user: user4)
+profile4.save
+
+
+
